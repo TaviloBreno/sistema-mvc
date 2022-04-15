905097e2d6265c0cc8befbbbaeabe2431935bc23
Tavilo Breno <breno_wk2@hotmail.com>
2022-04-15 15:20:18 -0300

    Iniciando o projeto sistema mvc

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php 
+require_once("enviroment.php");
+
+$config = [];
+
+if (ENVIROMENT == 'development') {
+	define('BASE_URL', "http://localhost/B7web/PHP_do_zero_ao_profissional/Modulo%2016%20-%20PHP%20super%20avan%c3%a7ado/mvc/");
+	//Banco de dados de desenvolvedor
+	$config['dbname'] = 'mvc';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+}else{
+	define('BASE_URL', "https://meusite.com.br");
+	//Banco de dados de produção
+	$config['dbname'] = 'mvc';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = '';
+}
+
+global $db;
+
+try {
+	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+} catch (Exception $e) {
+	echo "ERRO: ".$e->getMessage();
+	exit;
+}
