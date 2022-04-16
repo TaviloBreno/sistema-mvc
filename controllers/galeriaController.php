2295317fcd7c0619b7728cd25ad6e30032f00535
Tavilo Breno <breno_wk2@hotmail.com>
2022-04-15 21:09:30 -0300

    Criação do core, Controllers emodels

---
+++ controllers/galeriaController.php
@@ -0,0 +1,11 @@
+<?php 
+
+class galeriaController{
+	public function index(){
+		echo "Página de galeria";
+	}
+
+	public function abrir($id){
+		echo "Abrindo galeria: ".$id;
+	}
+}
---
+++ controllers/homeController.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+class homeController{
+
+	public function index(){
+		echo "Olá mundo!";
+	}
+
+	public function teste(){
+		echo "Este é um teste.";
+	}
+
+}
